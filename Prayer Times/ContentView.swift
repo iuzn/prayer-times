3b9183d3bc090b2cf7b9c43c3cd6140e8b0bdcb5
Ibrahim Uzun <info@ibrahimuzun.com>
2023-06-06 18:21:38 +0300

    Initial Commit

---
+++ Prayer Times/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Prayer Times
+//
+//  Created by ibrahim uzun on 6/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Prayer Times/Prayer_TimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prayer_TimesApp.swift
+//  Prayer Times
+//
+//  Created by ibrahim uzun on 6/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Prayer_TimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
